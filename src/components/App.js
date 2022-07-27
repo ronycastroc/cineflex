5500063e85c9e9e6e7d9a4b0dba357c14e4c9165
ronycastroc <castroroony@gmail.com>
2022-07-27 01:41:21 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MainPage from './MainPage'
+
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<MainPage />}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
